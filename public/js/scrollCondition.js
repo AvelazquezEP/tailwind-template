68e45c5391183f3afb374f7ce3cb9b0825fccf26
Avelazquez <avelazquez@abogadoericprice.com>
2024-01-18 16:34:20 -0800

    UPDATE: Media top, this is a small CTA and only can show when the customer is on the home of the website that mean only when can see the hero section (this is a slider)

--- public/js/scrollCondition.js
+++ public/js/scrollCondition.js
@@ -2,6 +2,7 @@ document.addEventListener("scroll", function () {
     var log = console.log;
     var scrollpos = window.scrollY;
     var nav = document.getElementById("navigation");
+    var mediaTop = document.getElementById("mediaTop");
     // scrollpos = window.scrollY;
 
     if (scrollpos > 10) {
@@ -9,8 +10,10 @@ document.addEventListener("scroll", function () {
         // nav.style.backgroundColor = 'black';
         // nav.classList.remove("bg-white");
         // nav.classList.add("bg-black");
+        mediaTop.classList.add("d-none");
     } else {
         log('Less than 10')
+        mediaTop.classList.remove("d-none");
         // nav.classList.remove("bg-black");
         // nav.classList.add("bg-white");
     }
